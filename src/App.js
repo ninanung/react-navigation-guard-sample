35c22c29f76e0f22fe98f5d675dfa088ead09744
Seungje Jun <ninanung@naver.com>
2018-11-20 13:59:30 +0900

    sample

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NavigationGuard from 'react-navigation-guard';
+import { Switch, BrowserRouter } from 'react-router-dom';
+
 import './App.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <h2>HOME</h2>
+    )
+  }
+}
+
+class Test extends Component {
+  render() {
+    return (
+      <h2>TEST</h2>
+    )
+  }
+}
+
+class Route extends Component {
+  returnBool = (path, url, params) => {
+    console.log(path);
+    console.log(url);
+    console.log(params);
+    if(params.test) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  ifTrue = (path, url, params) => {
+    console.log('true');
+  }
+
+  ifFalse = (path, url, params) => {
+    console.log('false');
+  }
+
+  render() {
+    return (
+      <Switch>
+        <NavigationGuard exact={true} component={Home} path="/" returnBool={this.returnBool} ifTrue={this.ifTrue} ifFalse={this.ifFalse} />
+        <NavigationGuard exact={true} component={Test} path="/test/:test" returnBool={this.returnBool} ifTrue={this.ifTrue} ifFalse={this.ifFalse} />
+      </Switch>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <BrowserRouter>
+          <Route/>
+        </BrowserRouter>
       </div>
     );
   }
