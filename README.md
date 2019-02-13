ad1fec1fc08e8d55b7bf89b55e1a62d7c328e5c7
Seungje Jun <ninanung@naver.com>
2019-02-13 16:17:08 +0900

    v1.0.5

--- README.md
+++ README.md
@@ -9,10 +9,17 @@ npm start
 ```  
 
 ## Result  
-As you can see in the `src/App.js` file. I code all the functions to do `console.log(something)`. If url parameter has `test` value, return true and if not, return false. So, in the `Home` component(first image), you can see "false" log.  
+As you can see in the `src/App.js` file. I write all the functions to do `console.log(something)`. If url parameter `test`'s value is also `"test"`, return true and if not, return false. So, in the `Home` component (first image), you can see "false" log.  
 ![](./public/home.png)  
-If you move to `localhost:3000/test/:test`, the `Test` component , you can see "true" log.  
+If you move to `localhost:3000/test/test`, the `Test` component , you can see "true" log.  
 ![](./public/test.png)  
+Hmm, why `Test` component didn't show text? Because, i'm using `blockRender()` function to block rendering. If you see the `App.js` file, i made all component `alert` when it rendered. But only when `returnBool` return true, it will not render the component. Let's see how it goes.  
+![](./public/test-popup.png)  
+Ok, if you move to `localhost:3000/test/abcd`, it shows you the alert. Cause `returnBool` return false. But, how about `localhost:3000/test/test` ?  
+![](./public/test-not.png)  
+Yes, it shows nothing. `blockRender()` function is used and component is not rendered.  
+![](./public/uncheck.png)  
+Last, you can use `Route` instead of `NavigationGuard` if you don't want to check some component. In the `App.js` file, `Uncheck` component is called by `Route` and show nothing in console.  
 
 ## You can use...  
 
--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import NavigationGuard from 'react-navigation-guard';
-import { Switch, BrowserRouter } from 'react-router-dom';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
 
 import './App.css';
 
 class Home extends Component {
   render() {
+    alert('Home component');
     return (
       <h2>HOME</h2>
     )
@@ -14,29 +15,39 @@ class Home extends Component {
 
 class Test extends Component {
   render() {
+    alert('Test component');
     return (
       <h2>TEST</h2>
     )
   }
 }
 
-class Route extends Component {
-  returnBool = (path, url, params) => {
-    console.log(path);
+class Uncheck extends Component {
+  render() {
+    alert('Uncheck component');
+    return (
+      <h2>UNCHECK</h2>
+    )
+  }
+}
+
+class Routes extends Component {
+  returnBool = (url, params) => {
     console.log(url);
     console.log(params);
-    if(params.test) {
+    if(params.test === 'test') {
       return true;
     } else {
       return false;
     }
   }
 
-  ifTrue = (path, url, params) => {
+  ifTrue = (url, params, blockRender) => {
+    blockRender() // block rendering component
     console.log('true');
   }
 
-  ifFalse = (path, url, params) => {
+  ifFalse = (url, params, blockRender) => {
     console.log('false');
   }
 
@@ -45,6 +56,7 @@ class Route extends Component {
       <Switch>
         <NavigationGuard exact={true} component={Home} path="/" returnBool={this.returnBool} ifTrue={this.ifTrue} ifFalse={this.ifFalse} />
         <NavigationGuard exact={true} component={Test} path="/test/:test" returnBool={this.returnBool} ifTrue={this.ifTrue} ifFalse={this.ifFalse} />
+        <Route exact component={Uncheck} path='/uncheck'/>
       </Switch>
     )
   }
@@ -55,7 +67,7 @@ class App extends Component {
     return (
       <div className="App">
         <BrowserRouter>
-          <Route/>
+          <Routes/>
         </BrowserRouter>
       </div>
     );
